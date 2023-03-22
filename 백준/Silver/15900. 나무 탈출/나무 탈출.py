065667d34004b8b1ea26365b947445896854330e
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2023-03-22 09:00:06 +0900

    [Silver I] Title: 나무 탈출, Time: 1632 ms, Memory: 133440 KB -BaekjoonHub

---
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+V = N-1
+edge = [[]*N for i in range(N+1)]
+visited = [-1] * (N+1)
+visited[1] = 0
+
+count = 0
+for i in range(V):
+    n, m = map(int, input().split())
+    edge[n].append(m)
+    edge[m].append(n)
+
+
+
+stack = [1]
+while stack:
+    r = stack.pop()
+    check = True
+    for next_node in edge[r]:
+        if visited[next_node] == -1:
+            visited[next_node] = visited[r] + 1
+            stack.append(next_node)
+            check = False
+
+    if check is True:
+        count += visited[r]
+
+if count % 2 == 0:
+    print('No')
+else:
+    print('Yes')
