68681cbda263ef0171f938dcac60691db86bf375
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2023-03-30 01:29:27 +0900

    [Silver II] Title: 나무 자르기, Time: 728 ms, Memory: 259740 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+top = 1
+low = sum(arr)
+
+while top <= low:
+    mid = (top + low) // 2
+    count = 0
+    for i in arr:
+        if i > mid:
+            count += i - mid
+
+    if count >= M:
+        top = mid + 1
+
+    else:
+        low = mid - 1
+
+print(low)
