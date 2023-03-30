6a3d2f156a236867b386719fb3ea12d90d025052
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2023-03-30 10:17:37 +0900

    [Silver II] Title: 랜선 자르기, Time: 472 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,23 @@
+K, N = map(int, input().split())
+
+lines = [int(input()) for _ in range(K)]
+
+start = 1
+end = max(lines)
+
+
+while start <= end:
+    count = 0
+    mid = (start + end) // 2
+    for line in lines:
+        count += line // mid
+
+    if count >= N:
+        start = mid + 1
+
+    else:
+        end = mid - 1
+
+
+
+print(end)
