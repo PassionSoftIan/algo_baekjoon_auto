9cffb9c09a43cba71bc4408b3258f29f96f3e583
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2023-04-05 02:27:41 +0900

    [Silver I] Title: 단절점과 단절선, Time: 276 ms, Memory: 46372 KB -BaekjoonHub

---
+++ 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+V = N-1
+tree = [[] for _ in range(N+1)]
+for i in range(V):
+    n, m = map(int, input().split())
+    tree[m].append(m)
+    tree[n].append(n)
+Q = int(input())
+for i in range(Q):
+    t, k = map(int, input().split())
+    if t == 1:
+        if len(tree[k]) == 1:
+            print('no')
+        else:
+            print('yes')
+    else:
+        print('yes')
