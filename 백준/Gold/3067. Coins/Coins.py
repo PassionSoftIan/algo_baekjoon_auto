66ba54084ed7f88fe716d84c3317f2935a4bb851
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2023-04-19 00:28:18 +0900

    [Gold V] Title: Coins, Time: 156 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/3067. Coins/Coins.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+Test_case = int(input())
+
+for tc in range(1, Test_case+1):
+    N = int(input())
+    coin = list(map(int, input().split()))
+    money = int(input())
+    
+    DP = [0] * (money+1)
+    DP[0] = 1
+    
+    for c in coin:
+        for i in range(1, money+1):
+            if i >= c:
+                DP[i] += DP[i-c]
+                
+    print(DP[money])
