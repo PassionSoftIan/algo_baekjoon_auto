96864394c5eb2e3a6f292fa1350d65c6b8a848ad
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2023-04-18 22:27:49 +0900

    [Gold V] Title: 평범한 배낭, Time: 4616 ms, Memory: 282468 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,18 +1,13 @@
 N, K = map(int, input().split())
 
-item = [list(map(int, input().split())) for _ in range(N)]
-
 DP_lst = [[0 for _ in range(K+1)] for _ in range(N+1)]
 
-for i in range(N+1):
-    for j in range(K+1):
-        if i == 0 or j == 0:
-            DP_lst[i][j] = 0
-
-        elif item[i-1][0] <= j:
-            DP_lst[i][j] = max(item[i-1][1] + DP_lst[i-1][j-item[i-1][0]],
-                               DP_lst[i-1][j])
-        else:
+for i in range(1, N+1):
+    weight, value = map(int, input().split())
+    for j in range(1, K+1):
+        if weight > j:
             DP_lst[i][j] = DP_lst[i-1][j]
+        else:
+            DP_lst[i][j] = max(value + DP_lst[i-1][j-weight], DP_lst[i-1][j])
 
 print(DP_lst[N][K])
