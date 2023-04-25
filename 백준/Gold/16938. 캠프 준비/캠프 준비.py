081f2b14a3fcd555164743c2520cd326178003cb
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2023-04-25 23:10:41 +0900

    [Gold V] Title: 캠프 준비, Time: 148 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.py
@@ -0,0 +1,14 @@
+N, L, R, X = map(int, input().split())
+level = list(map(int, input().split()))
+
+count = 0
+for i in range(1 << N):
+    arr = []
+    for j in range(N):
+        if i & (1 << j):
+            arr.append(level[j])
+    if L <= sum(arr) <= R and max(arr) - min(arr) >= X:
+        count += 1
+        # print(arr, i)
+
+print(count)
