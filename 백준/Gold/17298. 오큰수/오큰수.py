7aa9a1094e19e268b38ab03c3f8ba23f7c3f0cac
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2023-05-11 00:50:39 +0900

    [Gold IV] Title: 오큰수, Time: 1136 ms, Memory: 155560 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+
+arr = list(map(int, input().split()))
+lst = [-1] * N
+stack = [0]
+
+for i in range(1, N):
+    while stack and arr[i] > arr[stack[-1]]:
+        lst[stack.pop()] = arr[i]
+    stack.append(i)
+
+print(*lst)
