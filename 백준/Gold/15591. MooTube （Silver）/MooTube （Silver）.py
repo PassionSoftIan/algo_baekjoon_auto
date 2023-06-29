318816c6d49c903008258b5b0f9a48fd37bb0a5d
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2023-06-30 01:25:58 +0900

    [Gold V] Title: MooTube (Silver), Time: 1668 ms, Memory: 129804 KB -BaekjoonHub

---
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+N, Q = map(int, input().split())
+
+edge = [[] for _ in range(N+1)]
+
+for nc in range(N-1):
+    start, end, usado = map(int, input().split())
+    edge[start].append([end, usado])
+    edge[end].append([start, usado])
+
+for qc in range(Q):
+    K, num = map(int, input().split())
+    keep = [0] * (N + 1)
+    que = deque([[num, int(1e9)]])
+    count = 0
+    while que:
+        node, node_ussado = que.popleft()
+        keep[node] = 1
+        for check in edge[node]:
+            now = check[0]
+            ussado = check[1]
+            result = min(node_ussado, ussado)
+            if keep[now] == 0:
+                result = min(node_ussado, ussado)
+                if K <= result:
+                    count += 1
+                    que.append([now, ussado])
+                    keep[now] = 1
+    print(count)
