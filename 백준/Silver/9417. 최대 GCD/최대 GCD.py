74b1411bbeda8a3405a5bf5b52e518a0e79baa91
Ian <122499599+PassionSoftIan@users.noreply.github.com>
2024-01-08 22:35:36 +0900

    [Silver IV] Title: 최대 GCD, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9417. 최대 GCD/최대 GCD.py
+++ 백준/Silver/9417. 최대 GCD/최대 GCD.py
@@ -7,10 +7,14 @@ for tc in range(N):
     max_result = 0
     for i in range(M - 1):
         for j in range(i + 1, M):
-            for p in range(1, arr[i] + 1):
-                if arr[i] % p == 0 and arr[j] % p == 0:
-                    if max_result < p:
-                        max_result = p
+            a = arr[j]
+            b = arr[i]
+            while b != 0:
+                temp_a = a
+                a = b
+                b = temp_a % b
+            if max_result < a:
+                max_result = a
     result.append(max_result)
 
 for check in result:
